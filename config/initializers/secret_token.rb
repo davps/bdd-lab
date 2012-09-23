21b497c2d018ffe7d531718f4a2736f17db78951
davps <davidperez3010@gmail.com>
2012-09-23 15:26:32 -0400

    First commit
    This commit contains the Entry scaffolding, using rspec as default
    test framework in this rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webapp::Application.config.secret_token = '1ec0d3ab724b7a3a3822450df4778120157ac9839f5a95439b881cb979c67b594438d6bea084f54f5b8d47aeaa93cca355f85b0190a3a75656a30a54f98937a9'
